37a3bef17bb301b2f5e6dfd211c31a84b872ce02
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-12-10 14:18:44 +0100

    feat: newProduct in the state

--- react-test-shop/src/components/form-new-product.jsx
+++ react-test-shop/src/components/form-new-product.jsx
@@ -2,70 +2,75 @@ import React, { Component } from "react";
 
 class FormNewProduct extends Component {
   state = {
-    name: "",
-    price: "",
-    description: "",
-    imageUrl: "",
+    newProduct: {
+      name: "",
+      price: "",
+      description: "",
+      imageUrl: "",
+    },
   };
 
   handleChange = (e) => {
     const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
+    this.setState((prevState) => ({
+      newProduct: {
+        ...prevState.newProduct,
+        [name]: value,
+      },
+    }));
   };
 
-render() {
-  const { name, price, description, imageUrl } = this.state;
+  render() {
+    const { name, price, description, imageUrl } = this.state.newProduct;
 
-  return (
-    <form>
-      <label>
-        Produktname:
-        <input
-          name="name"
-          value={name}
-          onChange={this.handleChange}
-          type="text"
-          placeholder="z. B. Apfelsaft"
-        />
-      </label>
+    return (
+      <form>
+        <label>
+          Produktname:
+          <input
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+            type="text"
+            placeholder="z. B. Apfelsaft"
+          />
+        </label>
 
-      <label>
-        Preis:
-        <input
-          name="price"
-          value={price}
-          onChange={this.handleChange}
-          type="number"
-          placeholder="z. B. 3.99"
-        />
-      </label>
+        <label>
+          Preis:
+          <input
+            name="price"
+            value={price}
+            onChange={this.handleChange}
+            type="number"
+            placeholder="z. B. 3.99"
+          />
+        </label>
 
-      <label>
-        Beschreibung:
-        <input
-          name="description"
-          value={description}
-          onChange={this.handleChange}
-          type="text"
-          placeholder="z. B. Frischer Bio-Apfelsaft"
-        />
-      </label>
+        <label>
+          Beschreibung:
+          <input
+            name="description"
+            value={description}
+            onChange={this.handleChange}
+            type="text"
+            placeholder="z. B. Frischer Bio-Apfelsaft"
+          />
+        </label>
 
-      <label>
-        Bild-URL:
-        <input
-          name="imageUrl"
-          value={imageUrl}
-          onChange={this.handleChange}
-          type="text"
-          placeholder="z. B. /images/apfelsaft.png oder https://..."
-        />
-      </label>
-    </form>
-  );
-}
+        <label>
+          Bild-URL:
+          <input
+            name="imageUrl"
+            value={imageUrl}
+            onChange={this.handleChange}
+            type="text"
+            placeholder="z. B. /images/apfelsaft.png oder https://..."
+          />
+        </label>
+      </form>
+    );
+  }
 }
 
 export default FormNewProduct;
