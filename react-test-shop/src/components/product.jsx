fe13b06535c85d423fb268dfc5107eeebd34f583
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-12-22 08:43:41 +0100

    feat: Product as a functional component

--- react-test-shop/src/components/product.jsx
+++ react-test-shop/src/components/product.jsx
@@ -1,26 +1,23 @@
 import React, { Component } from "react";
 
-class Product extends Component {
-  state = {};
-  render() {
+const Product = ({ title, description, price, onAdd }) => {
     return (
       <div className="card" style={{ width: "18rem" }}>
         <img
           className="card-img-top"
-          src={`/assets/img/${this.props.title.toLowerCase()}.jpg`}
-          alt={`Image of ${this.props.title}`}
+          src={`/assets/img/${title.toLowerCase()}.jpg`}
+          alt={`Image of ${title}`}
         />
         <div className="card-body">
-          <h5 className="card-title">{this.props.title}</h5>
-          <p className="card-text">{this.props.description}</p>
-          <p>{this.props.price}€</p>
-          <button onClick={this.props.onAdd} className="btn btn-primary">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p>{price}€</p>
+          <button onClick={onAdd} className="btn btn-primary">
             Hinzufügen
           </button>
         </div>
       </div>
     );
-  }
-}
+  };
 
 export default Product;
