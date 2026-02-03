12be98a817e7a1f4e8987cf48da81c0ff325df23
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2026-02-03 11:52:36 +0100

    feat: aktive NavLinks

--- react-test-shop/src/components/navbar.jsx
+++ react-test-shop/src/components/navbar.jsx
@@ -1,23 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand bg-light border-bottom">
       <div className="container-fluid d-flex justify-content-between align-items-center">
-        <Link to="/products" className="navbar-brand fw-bold">
+        <NavLink to="/products" end className="navbar-brand fw-bold">
           Mein Shop
-        </Link>
+        </NavLink>
         <div className="menu-links d-flex gap-3 me-3">
-          <Link to="/products" className="nav-link px-0" data-text="Produkte">
-            Produkte
-          </Link>
-          <Link to="/products/new" className="nav-link px-0" data-text="Neues Produkt">
-            Neues Produkt
-          </Link>
-          <Link to="/cart" className="nav-link px-0" data-text="Warenkorb">
-            Warenkorb
-          </Link>
+          <div className="menu-item">
+            <NavLink
+              to="/products"
+              end
+              className="nav-link px-0"
+              data-text="Produkte"
+            >
+              Produkte
+            </NavLink>
+          </div>
+          <div className="menu-item">
+            <NavLink
+              to="/products/new"
+              className="nav-link px-0"
+              data-text="Neues Produkt"
+            >
+              Neues Produkt
+            </NavLink>
+          </div>
+          <div className="menu-item">
+            <NavLink to="/cart" className="nav-link px-0" data-text="Warenkorb">
+              Warenkorb
+            </NavLink>
+          </div>
         </div>
       </div>
     </nav>
